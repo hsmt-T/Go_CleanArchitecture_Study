1a661161ae5ecaa9792d6edf0816c2812f6d127a
hsmtDP-T <ittuyo.hashi244@gmail.com>
2026-01-27 01:35:24 +0900

    README.mdに全テスト実行の手順を追加し、CreateUserHandlerのテストを実装

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ https://zenn.dev/castingone_dev/articles/77ecac84226521
 
 https://github.com/GSabadini/go-clean-architecture
 
+#memo
 
+全テストを実行
+go test ./...
--- usecase/create_user.go
+++ usecase/create_user.go
@@ -14,10 +14,10 @@ type CreateUserInput struct {
 }
 
 type CreateUserOutput struct {
-	ID			string
-	Name		string
-	Email		string
-	CreatedAt	time.Time
+	ID			string		`json:"id"`
+	Name		string		`json:"name"`
+	Email		string		`json:"email"`
+	CreatedAt	time.Time	`json:"created_at"`
 }
 
 // UseCase インターフェイス
