a93ad431a6acb069aa86a813986f63f0db1d8f5e
hsmtDP-T <ittuyo.hashi244@gmail.com>
2026-01-26 23:42:32 +0900

    go.modとgo.sumにgolang.org/x/time v0.14.0を追加し、echo.goでミドルウェアのロガーとリカバーを有効化

--- infrastructure/router/echo.go
+++ infrastructure/router/echo.go
@@ -9,15 +9,17 @@ import (
 	"log"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func StartEcho() {
 	e := echo.New()
 
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	// ローカルDB
 	// repo := repository.NewUserMemoryRepository()
-
-
 	db, err := supabase.NewSupabase()
 
 	if err != nil {
